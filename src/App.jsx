7257a2757d6b6c2b0ad4bab44e7ea16cfdfc2fae
CarloZD <carlos.ruiz.l@tecsup.edu.pe>
2025-11-05 21:53:02 -0500

    Arreglos para la vista de Home y la barra

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-danger px-4">
-      <Link className="navbar-brand fw-bold" to="/">PokeDex</Link>
-      <div className="collapse navbar-collapse show">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Inicio</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/items">Pokémon</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contacto</Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+      <div className="container-fluid px-4">
+        <Link className="navbar-brand fw-bold" to="/">PokeDex</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/items">Pokémon</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contacto</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Home from './pages/Home'
 import Items from './pages/Items'
 import Contact from './pages/Contact'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
